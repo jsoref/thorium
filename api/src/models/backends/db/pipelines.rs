49610e2fbcb646585239241482a806931866f746
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-08-03 15:59:40 -0400

    spelling: likelihood
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- api/src/models/backends/db/pipelines.rs
+++ api/src/models/backends/db/pipelines.rs
@@ -352,7 +352,7 @@ pub async fn delete_all(
 /// Checks if a pipeline exists in the Redis backend after authentication
 ///
 /// Requiring a reference to a Group object obtained after authorization
-/// decreases the likelyhood of prematurely checking for the existence of
+/// decreases the likelihood of prematurely checking for the existence of
 /// the pipeline and leaking information to an unauthorized user
 ///
 /// # Arguments
