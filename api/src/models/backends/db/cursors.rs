f666bdb5029265fe5f9dedc171fc6b1afc9fd0f7
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-08-03 21:05:04 -0400

    spelling: throughout
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- api/src/models/backends/db/cursors.rs
+++ api/src/models/backends/db/cursors.rs
@@ -1682,7 +1682,7 @@ where
 {
     /// The ID for this cursor
     pub id: Uuid,
-    /// The data that is retained thoughout this cursors life
+    /// The data that is retained throughout this cursors life
     pub retain: SimpleScyllaCursorRetain,
     /// The max number of items to return at once
     pub limit: usize,
