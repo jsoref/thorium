20c8763682ea56a63816ffb4445616b974590906
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-08-03 21:31:06 -0400

    spelling: file
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- api/src/models/files.rs
+++ api/src/models/files.rs
@@ -2449,7 +2449,7 @@ impl CartedSample {
 #[cfg(feature = "client")]
 #[derive(Debug)]
 pub struct UncartedSample {
-    /// The fild handle containing our uncarted sample
+    /// The file handle containing our uncarted sample
     pub file: File,
 }
 
