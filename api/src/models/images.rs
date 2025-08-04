15671317ba9ac91fc8492fa28f4deb8fbc46d87f
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-08-03 21:27:56 -0400

    spelling: dependencies
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- api/src/models/images.rs
+++ api/src/models/images.rs
@@ -2439,11 +2439,11 @@ impl EphemeralDependencySettingsUpdate {
         self
     }
 
-    /// Add a new name to the list of dependencias to download
+    /// Add a new name to the list of dependencies to download
     ///
     /// # Arguments
     ///
-    /// * `name` - The name to add our list of dependencias to download
+    /// * `name` - The name to add our list of dependencies to download
     ///
     /// # Examples
     ///
@@ -2459,11 +2459,11 @@ impl EphemeralDependencySettingsUpdate {
         self
     }
 
-    /// Removes a name from the list of dependencias to download
+    /// Removes a name from the list of dependencies to download
     ///
     /// # Arguments
     ///
-    /// * `name` - The name to remove from our list of dependencias to download
+    /// * `name` - The name to remove from our list of dependencies to download
     ///
     /// # Examples
     ///
